f7c3645d6bb1293b481967239fed355faecd2697
James <36971481+Jimm1432@users.noreply.github.com>
2023-09-17 04:11:37 +0100

    V1.19

--- Src/config_selector.py
+++ Src/config_selector.py
@@ -1,9 +1,30 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 import shutil
 import os
 
-#You can change the file names to match your deck name if you wish.
+root = tk.Tk()
+root.title("Deck")
+# Set dark background
+root.configure(background='#575559')
+# Set window icon to png
+root.iconbitmap('calculon.ico')
+root.geometry("100x200")
+
+def move_window(event):
+    root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
+
+var = tk.StringVar()
+
+tk.Label(root, text="Select Deck").pack()
+
+tk.Radiobutton(root, text="Deck 1", variable=var, value="deck1").pack()
+tk.Radiobutton(root, text="Deck 2", variable=var, value="deck2").pack()
+tk.Radiobutton(root, text="Deck 3", variable=var, value="deck3").pack()
+tk.Radiobutton(root, text="Deck 4", variable=var, value="deck4").pack()
+tk.Radiobutton(root, text="Deck 5", variable=var, value="deck5").pack()
+
+# Define the copy_file function here
 def copy_file():
     try:
         if var.get() == "deck1":
@@ -19,26 +40,7 @@ def copy_file():
     except FileNotFoundError:
         messagebox.showwarning("Warning", "No such file.")
 
-root = tk.Tk()
-root.title("Deck")
-# Set dark background
-root.configure(background='#575559')
-# Set window icon to png
-#root.iconbitmap('calculon.ico')
-root.geometry("100x200")
-
-def move_window(event):
-    root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
-
-root.bind('<B1-Motion>', move_window)
-
-var = tk.StringVar()
-
-tk.Label(root, text="Select Deck").pack()
-
-tk.Radiobutton(root, text="Deck 1", variable=var, value="deck1").pack()
-tk.Radiobutton(root, text="Deck 2", variable=var, value="deck2").pack()
-tk.Radiobutton(root, text="Deck 3", variable=var, value="deck3").pack()
-tk.Radiobutton(root, text="Deck 4", variable=var, value="deck4").pack()
-tk.Radiobutton(root, text="Deck 5", variable=var, value="deck5").pack()
+# Create the "Submit" button with the copy_file command
 tk.Button(root, text="Submit", command=copy_file).pack()
+
+root.mainloop()
